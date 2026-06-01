0407735c5f3a313078938906986465064196ab8c
radouane <radouane.aitsaid01@gmail.com>
2026-06-01 10:10:40 +0100

    test: minimal .NET 6 project for fast pipeline testing

--- dotnet6-multi-pattern/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace Dotnet6MultiPattern.Models;
-
-public class Order
-{
-    public int Id { get; set; }
-    public string CustomerName { get; set; } = "";
-    public decimal Amount { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
